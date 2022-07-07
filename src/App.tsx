3e8881ca445ec9c8834559ba02c1799fa6a9055e
MarioAvilaJunior <juniormavila@hotmail.com>
2022-07-06 23:25:03 -0300

    Creation of a card for formatting and styling the meals list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Header } from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />;
+      <Meals />
+    </>
+  );
 };
 
 export default App;
