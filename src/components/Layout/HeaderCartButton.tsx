85b95fb536bd463867fbf4f9bf5945457c9fb80a
MarioAvilaJunior <juniormavila@hotmail.com>
2022-07-19 11:58:31 -0300

    Seeting animation for the cart button whenever an item is added or removed

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -5,13 +5,33 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = ({ onShowCart }: { onShowCart: () => void }) => {
   const cartCtx = React.useContext(CartContext);
+  const [buttonBump, setButtonBump] = React.useState<boolean>(false);
+
+  const { items } = cartCtx;
+
+  const buttonClasses = `${classes.button} ${buttonBump ? classes.bump : ""}`;
+
+  React.useEffect(() => {
+    /*  if (items.length === 0) {
+      return;
+    } */
+    setButtonBump(true);
+
+    const timer = setTimeout(() => {
+      setButtonBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   const numberOfCartItems = cartCtx.items.reduce((totalAmount, item) => {
     return totalAmount + item.amount;
   }, 0);
 
   return (
-    <button className={classes.button} onClick={onShowCart}>
+    <button className={buttonClasses} onClick={onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
